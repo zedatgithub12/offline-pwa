984d73e9f372acec3ee5ba5185785b3a8f9f6f3b
Zerihun Tegenu <zerihuntegenu5@gmail.com>
2025-01-23 18:31:44 +0300

    done hajj mini-app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ethiohajj.com",
+        port: "",
+        pathname: "/storage/photo/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Fallback.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Fallback = () => {
+  return (
+    <div>
+      <p className="text-lg text-red-500">
+        Something went wrong. Please try again.
+      </p>
+    </div>
+  );
+};
+
+export default Fallback;

923dcf2517b88f3df3b20a24c233111a368e8498
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2025-01-28 10:38:46 +0300

    pwa test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,58 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/, 
+      handler: "NetworkFirst", 
+      options: {
+        cacheName: "http-cache",
+        networkTimeoutSeconds: 15, 
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: /^\/_next\/.*$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "next-static-files",
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60, 
+        },
+      },
+    },
+    {
+      urlPattern: /^\/static\/.*$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-resources",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 7, 
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.(gstatic|googleapis)\.com\/.*/, 
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 365 * 24 * 60 * 60,
+        },
+      },
+    },
+  ],
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = withPWA({
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint during builds
   },
@@ -15,8 +66,4 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
-
-export default nextConfig;
-
-
+});

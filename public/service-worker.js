99903453a7b4fe954254423eb33bcad32d7a9f84
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2025-01-30 09:35:57 +0300

    added dynamic cache fallback page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "hajji-offline";
 
-//caching the static asset while installing the service worker in the browser
+// Caching the static assets during service worker installation
 async function cacheCoreAssets() {
   const cache = await caches.open(CACHE_NAME);
   return cache.addAll([
@@ -19,7 +19,7 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-// before actvating the service worker clear the old previous caches
+// Clear old caches before activating the service worker
 async function clearOldCaches() {
   const cacheNames = await caches.keys();
   return Promise.all(
@@ -34,29 +34,32 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
-//handle service worker operations
+// Handle network requests with caching strategies
 async function dynamicCaching(request) {
   const cache = await caches.open(CACHE_NAME);
-
   try {
     const response = await fetch(request);
     const responseClone = response.clone();
     await cache.put(request, responseClone);
     return response;
   } catch (error) {
+    // If request is an HTML page and network fails, show offline.html
+    if (request.headers.get("Accept").includes("text/html")) {
+      return caches.match("/offline.html");
+    }
     return caches.match(request);
   }
 }
 
 async function cacheFirstStrategy(request) {
+  const cache = await caches.open(CACHE_NAME);
+  const cachedResponse = await cache.match(request);
+
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
   try {
-    const cache = await caches.open(CACHE_NAME);
-    const cachedResponse = await cache.match(request);
-
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-
     const networkResponse = await fetch(request);
     const responseClone = networkResponse.clone();
     await cache.put(request, responseClone);
@@ -68,7 +71,8 @@ async function cacheFirstStrategy(request) {
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
-  if (event.request.mode === "navigate") {
+
+  if (request.mode === "navigate") {
     event.respondWith(cacheFirstStrategy(request));
   } else {
     event.respondWith(dynamicCaching(request));
